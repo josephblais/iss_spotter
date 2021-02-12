812b6683d34148ead48fe3d20bdfeb88e1c00d32
Joseph Blais <josephpierreblais@gmail.com>
2021-02-11 21:13:26 -0500

    implemented promise-based API calls

---
+++ iss_promised.js
@@ -0,0 +1,28 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const IP = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${IP}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const latitude = JSON.parse(body).latitude;
+  const longitude = JSON.parse(body).longitude;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
